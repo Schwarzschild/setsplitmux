d5d165a7dea3b3f96b7e6455763247b388ed7d96
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2020-10-14 13:21:03 -0400

    Setting up for Josh's computer.

--- setsplitmux.zsh
+++ setsplitmux.zsh
@@ -2,7 +2,7 @@ cd $HOME/dev/splitmux
 SPLITMUXDIR=$HOME/Dropbox/LIVESTREAMING/TechnicalFiles/"SPLITMUX - CUSTOM LAYOUTS"
 
 
-
+eval "$(pyenv init -)"
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 
 pyenv shell splitmux
