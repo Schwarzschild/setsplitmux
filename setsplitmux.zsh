1ed9f92dff5f3c7ec23f20d75102454dc4ea23f4
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2020-11-04 17:21:19 -0500

    Fix setsplitmux.zsh user var.

--- setsplitmux.zsh
+++ setsplitmux.zsh
@@ -2,7 +2,7 @@
 
 export SPLITMUX_IP=192.168.10.12
 export SPLITMUX_DIR=$HOME/Dropbox/LIVESTREAMING/TechnicalFiles/"SPLITMUX - CUSTOM LAYOUTS"
-export SPLIMUX_USER=marc
+export SPLITMUX_USER=marc
 export SPLITMUX_PW=marc
 
 
