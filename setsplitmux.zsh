e7f0980c0b332a573117d5c38b230dcf55ef461d
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2020-11-17 09:09:00 -0500

    Changed SPLITMUX_LAYOUTDIR.

--- setsplitmux.zsh
+++ setsplitmux.zsh
@@ -2,7 +2,7 @@
 
 export SPLITMUX_DIR=$HOME/dev/setsplitmux
 export SPLITMUX_IP=192.168.88.205
-export SPLITMUX_LAYOUTDIR=$HOME/Dropbox/LIVESTREAMING/TechnicalFiles/"SPLITMUX - CUSTOM LAYOUTS"
+export SPLITMUX_LAYOUTDIR=$HOME/Dropbox/LIVESTREAMING/TechnicalFiles/SplitmuxCustomLayouts
 export SPLITMUX_USER=marc
 export SPLITMUX_PW=marc
 
@@ -19,8 +19,9 @@ eval "$(pyenv init -)"
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 pyenv shell splitmux
 
+echo "About to run setsplitmux.py"
 python ./setsplitmux.py $@
 # python setsplitmux.py "2-shot C&D.xml"
 # python setsplitmux.py "BBoy_3+1(usk).xml"
 
-echo "two"
+echo "Finished."
