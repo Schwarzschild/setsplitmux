29b88e58309833336de8dba27eb013ef807ed7de
SPP <sppmacmini@SPPs-Mac-mini.local>
2020-12-24 17:58:58 -0500

    added usr/local/bin to path

--- setsplitmux.zsh
+++ setsplitmux.zsh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+export PATH=/usr/local/bin:$PATH
+
 export SPLITMUX_DIR=$HOME/dev/setsplitmux
 export SPLITMUX_IP=192.168.88.205
 export SPLITMUX_LAYOUTDIR=$HOME/Dropbox/LIVESTREAMING/TechnicalFiles/SplitmuxCustomLayouts
@@ -13,27 +15,17 @@ then
   export PCMD=$HOME/.pyenv/versions/3.8.2/envs/splitmux/bin/python
 else
   export PCMD=$HOME/.pyenv/shims/python
-  source $HOME/.zshrc
 fi
 
 cd $SPLITMUX_DIR
 
-#export PYENV_ROOT="$HOME/.pyenv"
-#eval "$(pyenv init -)"
-#export VIRTUAL_ENV=""
-#if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
-
+export PYENV_ROOT="$HOME/.pyenv"
+eval "$(pyenv init -)"
+export VIRTUAL_ENV=""
+if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 pyenv shell splitmux
-#pyenv activate splitmux
 
-echo "About to run setsplitmux.py" > $SPLITMUX_DIR/splitmux.out
+echo "About to run setsplitmux.py"
+python $SPLITMUX_DIR/setsplitmux.py $@
 
-pyenv versions >> $SPLITMUX_DIR/splitmux.out
-which python  >> $SPLITMUX_DIR/splitmux.out
-pip freeze >> $SPLITMUX_DIR/splitmux.out
-
-python $SPLITMUX_DIR/setsplitmux.py $@ >> $SPLITMUX_DIR/splitmux.out 2>$SPLITMUX_DIR/splitmux.err
-# python setsplitmux.py "2-shot C&D.xml"
-# python setsplitmux.py "BBoy_3+1(usk).xml"
-
-echo "Finished." >> splitmux.out
+echo "Finished."
